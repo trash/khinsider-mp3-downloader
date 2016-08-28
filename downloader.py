b236791edc50aa29497f7377f8423cc69dae8250
Stefan Valentin <khalilravanna@gmail.com>
2016-08-28 13:00:22 -0500

    Added console output. Takes url to start

--- downloader.py
+++ downloader.py
@@ -3,15 +3,23 @@ from urlparse import urlparse
 import urllib2, json
 from bs4 import BeautifulSoup
 
-base_dir = 'downloads'
-dir_name = base_dir + '/' + 'disgaea 4 ost'
-url = 'http://downloads.khinsider.com/game-soundtracks/album/disgaea-4-a-promise-unforgotten-soundtrack'
+print 'Please input link in quotes to album on khinsider.'
+print 'Example input (including quotes): \'http://downloads.khinsider.com/game-soundtracks/album/disgaea-4-a-promise-unforgotten-soundtrack\''
+url = input('Url: ')
 
-# Create the dir
+base_dir = 'downloads'
+url_parts = url.split('/')
+dir_name = base_dir + '/' + url_parts[len(url_parts) - 1]
+
+# Create directories
 if not os.path.exists(base_dir):
-    os.makedirs(base_dir)
+	print '[info] creating directory: ' + base_dir
+	os.makedirs(base_dir)
 if not os.path.exists(dir_name):
-    os.makedirs(dir_name)
+	print '[info] creating directory: ' + dir_name
+	os.makedirs(dir_name)
+
+print '[info] crawling for links...'
 
 soup = BeautifulSoup(urllib2.urlopen(url))
 
@@ -23,6 +31,8 @@ for anchor in anchors:
 	if 'mp3' in href:
 		links.append(href)
 
+print '[info] links acquired'
+
 downloaded_mp3s = {}
 
 for link in links:
@@ -33,8 +43,9 @@ for link in links:
 		downloaded_mp3s[mp3_url] = True
 		parts = mp3_url.split('/')
 		file_name = parts[len(parts) - 1]
+
+		print '[...] ' + file_name
 		mp3file = urllib2.urlopen(mp3_url)
 		with open(dir_name + '/' + file_name,'wb') as output:
 			output.write(mp3file.read())
-			print 'finished downloading ' + file_name
-			break
+			print '[downloaded] ' + file_name
