9fd816d67399dcc41df9f6d854f65e9c05a93aa2
Stefan Valentin <khalilravanna@gmail.com>
2016-08-28 13:10:22 -0500

    Add handling for when files already downloaded

--- downloader.py
+++ downloader.py
@@ -7,6 +7,9 @@ print 'Please input link in quotes to album on khinsider.'
 print 'Example input (including quotes): \'http://downloads.khinsider.com/game-soundtracks/album/disgaea-4-a-promise-unforgotten-soundtrack\''
 url = input('Url: ')
 
+# For testing
+# url = 'http://downloads.khinsider.com/game-soundtracks/album/disgaea-4-a-promise-unforgotten-soundtrack'
+
 base_dir = 'downloads'
 url_parts = url.split('/')
 dir_name = base_dir + '/' + url_parts[len(url_parts) - 1]
@@ -25,6 +28,7 @@ soup = BeautifulSoup(urllib2.urlopen(url))
 
 anchors = soup.find_all('a')
 
+# Acquire links
 links = []
 for anchor in anchors:
 	href = anchor.get('href')
@@ -35,6 +39,8 @@ print '[info] links acquired'
 
 downloaded_mp3s = {}
 
+# http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
+# Iterate through links, grab the mp3s, and download them
 for link in links:
 	link_soup = BeautifulSoup(urllib2.urlopen(link))
 	audio = link_soup.find('audio')
@@ -44,8 +50,25 @@ for link in links:
 		parts = mp3_url.split('/')
 		file_name = parts[len(parts) - 1]
 
-		print '[...] ' + file_name
 		mp3file = urllib2.urlopen(mp3_url)
-		with open(dir_name + '/' + file_name,'wb') as output:
-			output.write(mp3file.read())
-			print '[downloaded] ' + file_name
+
+		# get file size
+		meta = mp3file.info()
+		file_size = float(meta.getheaders("Content-Length")[0]) / 1000000
+
+		file_on_disk_path = dir_name + '/' + file_name
+		# check if file already exists
+		file_already_downloaded = False
+		if os.path.exists(file_on_disk_path):
+			stat = os.stat(file_on_disk_path)
+			file_already_downloaded = round(float(stat.st_size) / 1000000, 2) == round(file_size, 2)
+
+		# It exists but isn't already the same size
+		if not file_already_downloaded:
+			print '[downloading] ' + file_name + ' [%.2f' % file_size + 'MB]'
+
+			with open(file_on_disk_path,'wb') as output:
+				output.write(mp3file.read())
+				print '[done] "' + file_name + '"'
+		else:
+			print '[skipping] "' + file_name + '"" already downloaded.'
