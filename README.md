7da3cfe8e252975762e4ce21fa05afbb757016b2
Stefan Valentin <khalilravanna@gmail.com>
2020-06-21 19:33:14 -0400

    Updated to python3. Thanks /u/LiquidDoggo!
    - Updated readme.md as well to reflect much simpler process to run

--- README.md
+++ README.md
@@ -3,21 +3,18 @@
 A script to crawl `http://downloads.khinsider.com/` for game soundtracks and download them. Downloads will be placed inside a `/downloads` directory inside the repo. Individual directories for each album will be generated automatically off the url name.
 
 ## Install
-1. Install python from python.org if you haven't already. This will install `pip` so it can be nice to do either way.
-2. `$ pip install virtualenv`
-3. Start a new terminal instance inside the repository directory (`khinsider-mp3-downloader/`) so you can call `virtualenv`
-4. `$ virtualenv venv`
-5. `$ source venv/bin/activate`
-6. `$ pip install -r requirements.txt`
+The only thing you need to install is python3: https://www.python.org/downloads/
+
+Tested and working with python 3.8.
 
 ## How To Use
 
 ### `inputs.txt`
 
-Update the `inputs.txt` in the repo with a list of links, one link per line, and then run the script `$ python downloader.py`.
+Update the `inputs.txt` in the repo with a list of links, one link per line, and then run the script `$ python3 downloader.py`.
 The repo includes a properly formatted `inputs.txt` for reference.
 
 ### Input A URL Via CLI
 
-If you'd prefer to manually enter URLs you can delete `inputs.txt` and then simply run `$ python downloader.py` from inside the repo and enter a link like 'http://downloads.khinsider.com/game-soundtracks/album/disgaea-3-raspberyl' (including the quotes) when prompted in the command line and hit enter.
+If you'd prefer to manually enter URLs you can delete `inputs.txt` and then simply run `$ python3 downloader.py` from inside the repo and enter a link like 'http://downloads.khinsider.com/game-soundtracks/album/disgaea-3-raspberyl' (including the quotes) when prompted in the command line and hit enter.
 
--- downloader.py
+++ downloader.py
@@ -1,6 +1,6 @@
 import os
-from urlparse import urlparse
-import urllib2, json
+import urllib.parse as urlparse
+import urllib.request as urllib2, json
 from bs4 import BeautifulSoup
 
 BASE_URL = 'https://downloads.khinsider.com'
@@ -13,9 +13,9 @@ def validate_url (url):
 def fetch_from_url (url):
 	valid = validate_url(url)
 	if not valid:
-		print '[error] Invalid url: ' + url
+		print('[error] Invalid url: ' + url)
 		return
-	print '[info] Url found: ' + url
+	print('[info] Url found: ' + url)
 
 	base_dir = 'downloads'
 	url_parts = url.split('/')
@@ -23,13 +23,13 @@ def fetch_from_url (url):
 
 	# Create directories
 	if not os.path.exists(base_dir):
-		print '[info] creating directory: ' + base_dir
+		print('[info] creating directory: ' + base_dir)
 		os.makedirs(base_dir)
 	if not os.path.exists(dir_name):
-		print '[info] creating directory: ' + dir_name
+		print ('[info] creating directory: ' + dir_name)
 		os.makedirs(dir_name)
 
-	print '[info] crawling for links...'
+	print('[info] crawling for links...')
 
 	soup = BeautifulSoup(urllib2.urlopen(url))
 
@@ -47,18 +47,18 @@ def fetch_from_url (url):
 			if href not in songMap:
 				songMap[href] = anchor.string
 	if not songMap:
-		print '[error] No links found for the url. Double check that the url is correct and try again.'
-		print '[error] url: ' + url
+		print('[error] No links found for the url. Double check that the url is correct and try again.')
+		print('[error] url: ' + url)
 		return
 
-	print '[info] ' + str(len(songMap)) + ' links acquired'
+	print('[info] ' + str(len(songMap)) + ' links acquired')
 
 	# Map so we don't download duplicate links on the page
 	downloaded_mp3s = {}
 
 	# http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
 	# Iterate through links, grab the mp3s, and download them
-	for href, song_name in songMap.iteritems():
+	for href, song_name in songMap.items():
 		link_soup = BeautifulSoup(urllib2.urlopen(href))
 		audio = link_soup.find('audio')
 		mp3_url = audio.get('src')
@@ -71,7 +71,7 @@ def fetch_from_url (url):
 
 			# get file size
 			meta = mp3file.info()
-			file_size = float(meta.getheaders("Content-Length")[0]) / 1000000
+			file_size = float(meta.get("Content-Length")) / 1000000
 
 			file_on_disk_path = dir_name + '/' + file_name
 			# check if file already exists
@@ -82,23 +82,23 @@ def fetch_from_url (url):
 
 			# It exists but isn't already the same size
 			if not file_already_downloaded:
-				print '[downloading] ' + file_name + ' [%.2f' % file_size + 'MB]'
+				print('[downloading] ' + file_name + ' [%.2f' % file_size + 'MB]')
 
 				with open(file_on_disk_path,'wb') as output:
 					output.write(mp3file.read())
-					print '[done] "' + file_name + '"'
+					print('[done] "' + file_name + '"')
 			else:
-				print '[skipping] "' + file_name + '"" already downloaded.'
+				print('[skipping] "' + file_name + '"" already downloaded.')
 
 input_file_name = 'inputs.txt'
 if os.path.exists(input_file_name):
-	print '[info] Input file found. Parsing for links...'
+	print('[info] Input file found. Parsing for links...')
 	file = open(input_file_name, 'r')
 	for line in file:
 		fetch_from_url(line)
 else:
-	print 'Please input link in quotes to album on khinsider.'
-	print 'Example input (including quotes): \'http://downloads.khinsider.com/game-soundtracks/album/disgaea-4-a-promise-unforgotten-soundtrack\''
+	print('Please input link in quotes to album on khinsider.')
+	print('Example input (including quotes): \'http://downloads.khinsider.com/game-soundtracks/album/disgaea-4-a-promise-unforgotten-soundtrack\'')
 	url = input('Url: ')
 	fetch_from_url(url)
 
